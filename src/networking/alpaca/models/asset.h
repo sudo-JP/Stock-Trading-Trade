db2b1c0541d81a82835c6443244469fae796651e
Jason Phan <jase.phan@mail.utoronto.ca>
2025-10-16 17:08:25 -0400

    asset struct

---
+++ src/networking/alpaca/models/asset.h
@@ -0,0 +1,21 @@
+#include <string>
+#include "status.h"
+#ifndef _ASSET_H_
+#define _ASSET_H_ 
+
+struct Asset {
+    std::string id;
+    std::string asset_class;
+    std::string exchange;
+    std::string symbol; 
+    std::string name; 
+    Status status; 
+    bool tradeable;
+    bool marginable;
+    bool shortable;
+    bool easy_to_borrow; 
+    bool fractionable;
+};
+
+
+#endif
