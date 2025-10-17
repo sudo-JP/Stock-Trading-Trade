9b15f7d3499586edae91e1043185ee0849f0f4ab
Jason Phan <jase.phan@mail.utoronto.ca>
2025-10-17 16:25:34 -0400

    tcp client setup

---
+++ src/networking/tcp/client/tcp_client.cpp
@@ -0,0 +1,3 @@
+#include "tcp_client.h"
+
+
---
+++ src/networking/tcp/client/tcp_client.h
@@ -0,0 +1,26 @@
+#include <boost/asio.hpp>
+#ifndef _TCP_CLIENT_H
+#define _TCP_CLIENT_H
+
+using boost::asio::ip::tcp;
+
+class TCPClient {
+    public: 
+        TCPClient(const std::string host, const std::string port) : host(host), port(port), 
+        io_context(), resolver(io_context), socket(io_context) {
+            boost::asio::connect(socket, resolver.resolve(host, port)); 
+        };
+
+        bool send_data(std::string data); 
+
+        void disconnect(); 
+
+    private: 
+        const std::string host;
+        const std::string port; 
+        tcp::socket socket; 
+        tcp::resolver resolver; 
+        boost::asio::io_context io_context;
+}; 
+
+#endif
