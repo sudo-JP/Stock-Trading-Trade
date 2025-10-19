18affd58b21bf9824b049763a5f90ef8916938a0
Jason Phan <jase.phan@mail.utoronto.ca>
2025-10-19 02:36:30 -0400

    setting up binary protocol

--- src/networking/tcp/common/protocol.h
+++ src/networking/tcp/common/protocol.h
@@ -12,4 +12,51 @@ struct TCPMessage {
 std::string serialize(const TCPMessage &msg);
 TCPMessage deserialize(const std::string &data);
 
+// Binary Protocol 
+
+#pragma pack(push, 1)
+
+struct BinaryMessage {
+    uint32_t sql_command; 
+    uint32_t table; 
+    uint64_t timestamp; 
+    uint32_t data_size; 
+    //char payload[];
+};
+
+enum SQLCommand {
+    INSERT, 
+    SELECT, 
+    UPDATE,
+    DELETE 
+}; 
+
+enum SQLTable {
+    ACCOUNT,
+    ORDER, 
+    POSITION, 
+    INSTRUMENT, 
+}; 
+
+// Times are represented in nanoseconds 
+
+struct OrderBinaryPayload {
+    char id[64];
+    char client_order_id[64];
+    uint64_t created_at;
+    uint64_t updated_at;
+    uint64_t submitted_at;
+    uint64_t filled_at;
+    
+    char symbol[8];
+    char side[4];
+    char type[16];
+    uint64_t time_in_force;
+
+    uint32_t filled_qty;
+    float filled_avg_price;
+};
+
+#pragma pack(pop)
+
 #endif 
