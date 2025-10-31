d1ffdb3333e8696c4a9bfe405e11e2eca724278e
Jason Phan <jase.phan@mail.utoronto.ca>
2025-10-31 16:45:44 -0400

    correction for position struct

--- src/networking/alpaca/models/position_model.h
+++ src/networking/alpaca/models/position_model.h
@@ -1,3 +1,4 @@
+#include <cstdint>
 #ifndef _POSITION_MODEL_H_
 #define _POSITION_MODEL_H_
 
@@ -5,16 +6,16 @@
 struct PositionBinaryPayload {
     char asset_id[64];
     char symbol[16];
-    char exchange[16];
+    char exchange[8];
     char asset_class[16];
+    char side[8];
 
-    float qty;                 // positive = long, negative = short
-    char side[8];              // "long" or "short"
+    float qty;
+    double qty_available;
     float avg_entry_price;
     float market_value;
     float cost_basis;
 
-    // Optional / derived
     float unrealized_pl;
     float unrealized_plpc;
     float unrealized_intraday_pl;
@@ -25,6 +26,8 @@ struct PositionBinaryPayload {
     float change_today;
 
     bool asset_marginable;
+    int64_t last_update;  // nanoseconds
 };
 #pragma pack()
+
 #endif 
